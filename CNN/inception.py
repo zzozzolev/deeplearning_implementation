e4befae9ee3fb0cb63a96843da67579afcb0cbf4
zzozzolev <gpalektzja@gmail.com>
2018-12-16 21:30:43 +0900

    Declare placeholders, logits, preds, loss, optimizer, and acc

--- CNN/inception.py
+++ CNN/inception.py
@@ -196,3 +196,13 @@ def network(x, dropout, version='naive'):
     x = fc(x, len(n_hidden)-1)
     
     return x
+
+X, Y, keep_prob, lr_rate_placeholder = get_placeholder(mnist.train.images, mnist.train.labels)
+
+logits = network(X, keep_prob, 'reduction')
+preds = tf.nn.softmax(logits)
+
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y))
+optimizer = tf.train.AdamOptimizer(lr_rate_placeholder).minimize(loss)
+
+acc = tf.reduce_mean(tf.to_float(tf.equal(tf.argmax(pred, 1), tf.argmax(Y, 1)))) 
