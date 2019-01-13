2237cea6debb50ae3847a46ebc7d242ae6b96145
zzozzolev <gpalektzja@gmail.com>
2019-01-13 15:27:06 +0900

    Declare functions

--- rnn.py
+++ rnn.py
@@ -15,3 +15,73 @@ num_units = 256
 n_hidden = num_units * 2
 n_class = 10
 dropout = 0.9
+
+def get_placeholder(x, y):
+    X = tf.placeholder(tf.float32, [None, x.shape[-1]])
+    Y = tf.placeholder(tf.float32,[None, y.shape[-1]])
+    keep_prob = tf.placeholder(tf.float32)
+    lr_rate_placeholder = tf.placeholder(tf.float32)
+    
+    return X, Y, keep_prob, lr_rate_placeholder
+
+def fc(x, n_hidden, keep_prob):
+    with tf.variable_scope('fc'+str(n_hidden)):
+        x = tf.contrib.layers.layer_norm(x)
+        w = tf.get_variable("w", [x.get_shape().as_list()[-1], n_hidden],
+                                   initializer=tf.contrib.layers.xavier_initializer())
+        b = tf.get_variable("b", [n_hidden], initializer=tf.contrib.layers.xavier_initializer())
+            
+        output = tf.matmul(x, w) + b
+        
+        if n_hidden != n_class:
+            output = tf.nn.dropout(output, keep_prob)
+        
+        print(output)
+        
+        return output
+
+def rnn(x, n_layer, time_steps, num_units, keep_prob):
+    with tf.variable_scope('rnn'):
+        x = tf.contrib.layers.layer_norm(x)
+        
+        cells = []
+        for _ in range(n_layer):
+            cell = tf.contrib.rnn.BasicLSTMCell(num_units)
+            cell = tf.contrib.rnn.DropoutWrapper(cell, keep_prob)
+            cells.append(cell)
+        
+        multi_cells = tf.contrib.rnn.MultiRNNCell(cells)
+        
+        # [batch_size, sequence_length, num_units]
+        outputs, _ = tf.nn.dynamic_rnn(
+            multi_cells, x, dtype=tf.float32) 
+        
+        return outputs
+
+def network(x, n_layer, num_steps, num_units, keep_prob):
+    
+    x = tf.reshape(x, [-1, 28, 28])
+    
+    rnn_outputs = rnn(x, n_layer, num_steps, num_units, keep_prob)
+    print(rnn_outputs)
+    
+    fc_input = tf.reshape(rnn_outputs, [-1, num_units])
+    print(fc_input)
+    
+    fc_output = fc(fc_input, n_hidden, keep_prob)
+    activated = tf.nn.relu(fc_output)
+    print(activated)
+    
+    output = fc(activated, n_class, keep_prob)
+    
+    reshaped = tf.reshape(output, [-1, num_steps, n_class])
+    print(reshaped)
+    
+    # [num_steps, batch_size, num_units]
+    transposed = tf.transpose(reshaped, [1, 0, 2])
+    print(transposed)
+    
+    last_step_outputs = transposed[num_steps-1]
+    print(last_step_outputs)
+    
+    return last_step_outputs
